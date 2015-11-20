d67e019e017077425d9af3711c7c09c804d75052
LuckyMagpie <diego.sse7@gmail.com>
2015-11-20 00:29:57 -0600

    Activate ANN

--- activate.py
+++ activate.py
@@ -1,7 +1,13 @@
 import cPickle
+import librosa
 from sys import argv
 
 with open("netFile.pkl", "rb") as arch:
     net = cPickle.load(arch)
 
-script, sound, num = argv
+
+y,sr = librosa.load(argv[1], duration=10.0)
+
+y = net.activate(y)
+librosa.output.write_wav("net.wav", y, sr)
+
