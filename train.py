71155d6a3a6ac55c076a7b23d4e6cad6e3776f80
LuckyMagpie <diego.sse7@gmail.com>
2015-11-19 22:37:17 -0600

    Training

--- train.py
+++ train.py
@@ -8,18 +8,17 @@ import wienerlayer
 import adaptfilt
 import backprop
 
-y, sr = librosa.load("./sample/asd.wav", duration=15.0)
-y2, sr2 = librosa.load("./sample/asd1.wav", duration=15.0)
+ds = SupervisedDataSet(220500, 220500)
+for i in xrange(5):
+    y, sr = librosa.load("./sample/" +str(i)+ "/" +str(i)+ ".mp3", duration=10.0)
+    y2, sr2 = librosa.load("./sample/" +str(i)+ "/R" +str(i)+ ".mp3", duration=10.0)
+    ds.addSample(y, y2)
 
-net = buildNetwork(len(y), 35, 30, 25, 20, 15, 13, 6, 3, len(y2), hiddenclass=TanhLayer,outclass=wienerlayer.WienerLayer)
-ds = SupervisedDataSet(len(y), len(y2))
-
-ds.addSample(y, y2)
-# ...
+net = buildNetwork(220500, 35, 30, 25, 20, 15, 13, 6, 3, 220500, hiddenclass=TanhLayer,outclass=wienerlayer.WienerLayer)
 
 trainer = backprop.BackpropTrainerWiener(net, ds)
 
-for i in xrange(1):
+for i in xrange(200):
     print trainer.train()
 
 with open("netFile.pkl", "wb") as arch:
